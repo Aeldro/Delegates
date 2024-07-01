7f494e3c8a0b22950e853347d85304a6dfbd47ce
Aeldro <nodepuydt@gmail.com>
2024-07-01 12:04:55 +0200

    Ajoutez des fichiers projet.

---
+++ Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMoney money = ShowDollar;
+            Console.WriteLine(money(10));
+            money = ShowEuro;
+            Console.WriteLine(money(10));
+        }
+
+        public delegate string ShowMoney(int x);
+        public static string ShowDollar(int x)
+        {
+            return x.ToString() + "$";
+        }
+        public static string ShowEuro(int x)
+        {
+            return x.ToString() + "€";
+        }
+    }
+}
+
